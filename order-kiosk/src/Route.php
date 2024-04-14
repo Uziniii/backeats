a5f8e344f928c58a4f133a4045365fd3414f5c2b
Uzini <uzini.911@gmail.com>
2024-04-14 19:44:37 +0200

    first commit

---
+++ order-kiosk/src/Route.php
@@ -0,0 +1,34 @@
+<?php
+namespace Boutique;
+
+class Route {
+  private $path;
+  private $callable;
+  private $matches = [];
+  private $params = [];
+
+  public function __construct($path, $callable) {
+    $this->path = trim($path, '/');
+    $this->callable = $callable;
+  }
+
+  public function match($url) {
+    $url = trim(parse_url($url, PHP_URL_PATH), '/');
+    $path = preg_replace('#:([\w]+)#', '([^/]+)', $this->path);
+    $regex = "#^$path$#i";
+    if(!preg_match($regex, $url, $matches)){
+      return false;
+    }
+    array_shift($matches);
+    $this->matches = $matches;
+    return true;
+  }
+
+  public function call() {
+    $rep = explode("@", $this->callable);
+    $controller = "Boutique\\controllers\\".$rep[0];
+    $controller = new $controller();
+    return call_user_func_array([$controller, $rep[1]], $this->matches);
+  }
+}
+
