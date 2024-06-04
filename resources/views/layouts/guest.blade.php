2696a21d4975f966dbb9a397ff1b445d1d77368d
MatisBevilacqua <mbevilacqua@edenschool.fr>
2024-06-04 13:54:35 +0200

    add: logo of backeats

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -18,7 +18,7 @@
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
                 <a href="/" wire:navigate>
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                    <img class="mb-[20px]" src="{{asset("assets/imageCss/logo_remove_bg.png")}}" alt="logo du restaurant">
                 </a>
             </div>
 
