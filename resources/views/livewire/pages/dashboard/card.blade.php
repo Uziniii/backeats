1891be8eaa96051862adb763f4ba06b1310f2728
Uzini <uzini.911@gmail.com>
2024-06-11 16:36:41 +0200

    fix: products

--- resources/views/livewire/pages/dashboard/card.blade.php
+++
@@ -1,3 +0,0 @@
-<article wire:navigate class="w-40 h-40 flex justify-center items-center border bg-white border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">
-    <img src="{{Storage::url($data['image'])}}">
-</article>
--- resources/views/livewire/pages/dashboard/products.blade.php
+++ resources/views/livewire/pages/dashboard/products.blade.php
@@ -11,6 +11,18 @@ new #[Layout('layouts.app')] class () extends Component {
     {
         $this->products = Item::all();
     }
+
+    public function delete($id)
+    {
+        $item = Item::findOrFail($id);
+
+        // $this->authorize('delete', $item);
+
+        $item->delete();
+
+        return $this->redirectRoute("products");
+
+    }
 }; ?>
 
 <x-slot name="header">
@@ -31,7 +43,15 @@ new #[Layout('layouts.app')] class () extends Component {
                 <img src="{{URL::asset('assets/plus.png')}}" width="50px">
             </article>
             @foreach ($products as $product)
-                <livewire:pages.dashboard.card type="item" :data="$product"/>
+                <article  class="w-40 h-40 flex flex-col border bg-white border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">
+                    <div class="flex items-center justify-between p-1">
+                        <h1 class=" text-xl">{{$product['name']}}</h1>
+                        <button wire:click="delete({{ $product->id }})" class="w-6 h-6 bg-red-600 rounded-md flex justify-center items-center">
+                            <img src="{{URL::asset('assets/trash.png')}}" width="20px" class="h-5">
+                        </button>
+                    </div>
+                    <div style="background-image: url({{Storage::url($product['image'])}})" class="flex h-4/6 bg-contain bg-no-repeat w-full items-center justify-center bg-center"></div>
+                </article>
             @endforeach
         </div>
     </div>
